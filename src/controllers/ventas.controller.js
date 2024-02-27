5c5812c675adbd1c27d01739828606eee360416c
lucas leiro <lucasleiroa@gmail.com>
2024-02-27 17:37:18 -0300

    agregando cart parte 2 termiando el cart

--- src/controllers/ventas.controller.js
+++ src/controllers/ventas.controller.js
@@ -1,16 +1,22 @@
 import { getConecction } from "../database/conecction.js"
 import sql from 'mssql'
 
+var fecha  = new Date()
+var day = fecha.getDate();
+var mes = fecha.getMonth();
+var anio = fecha.getFullYear();
 
+var today = `${day}/${mes}/${anio}`
 
 export const getVentas = async (req,res) => {
 
   
-
-
+   
     const pool = await getConecction();
     const result = await pool.request().query('select * from categoria inner join articulo on categoria.categoria_id = articulo.categoria_id'); 
-    res.render('venta/venta.ejs' , {data: result.recordset})
+    const ventas = await pool.request().query('select * from venta'); 
+ 
+    res.render('venta/venta.ejs' , {data: result.recordset ,fecha:today ,numFact:ventas.recordset.NumFactura + 1})
   
    
 }
@@ -21,5 +27,14 @@ export const search = async (req , res) =>{
     const pool = await getConecction();
     const artSearch = req.body.busqueda;
     const result = await pool.request().query('select * from categoria inner join articulo on categoria.categoria_id = articulo.categoria_id where articulo_nombre LIKE '+"'"+artSearch+"%'")
-    res.render('venta/venta.ejs' , {data: result.recordset})
+    res.render('venta/venta.ejs' , {data: result.recordset , fecha:today})
+}
+
+
+export const searchClient = async(req , res) =>{
+    const pool = await getConecction();
+    const clientSearch = req.body.cliente;
+    const result = await pool.request().query('select * from cliente where cliente_nombre LIKE '+"'"+clientSearch+"%'");
+    res.render('venta/ventaClients.ejs',{clientes:result.recordset})
+
 }
--- src/routes/ventas.routes.js
+++ src/routes/ventas.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import {getVentas, search} from '../controllers/ventas.controller.js'
+import {getVentas, search,searchClient} from '../controllers/ventas.controller.js'
 
 const router = Router();
 
 router.get("/ventas",getVentas);
 router.post("/ventas/filter",search);
+router.post("/ventas/clientes",searchClient)
 
 
 export default router;
